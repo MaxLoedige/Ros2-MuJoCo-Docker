b73d9b8d35e3bf8ca26f3b9ee536f8b00c2e16ac
robotics_workstation_4 <robotics@hrc4.workstation>
2025-09-09 12:24:06 +0200

    removed submodule replaced with in-tree files

---
+++ src/mujoco_ros2_control/mujoco_ros2_control/src/mujoco_ros2_control_node.cpp
@@ -0,0 +1,260 @@
+// Copyright (c) 2025 Sangtaek Lee
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "mujoco/mujoco.h"
+#include "rclcpp/rclcpp.hpp"
+
+#include "mujoco_ros2_control/mujoco_cameras.hpp"
+#include "mujoco_ros2_control/mujoco_rendering.hpp"
+#include "mujoco_ros2_control/mujoco_ros2_control.hpp"
+
+#define MUJOCO_PLUGIN_DIR "mujoco_plugin"
+
+// MuJoCo data structures
+mjModel *mujoco_model = nullptr;
+mjData *mujoco_data = nullptr;
+
+std::string getExecutableDir() {
+#if defined(_WIN32) || defined(__CYGWIN__)
+  constexpr char kPathSep = '\\';
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    DWORD buf_size = 128;
+    bool success = false;
+    while (!success) {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if (!realpath) {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      DWORD written = GetModuleFileNameA(nullptr, realpath.get(), buf_size);
+      if (written < buf_size) {
+        success = true;
+      } else if (written == buf_size) {
+        // realpath is too small, grow and retry
+        buf_size *=2;
+      } else {
+        std::cerr << "failed to retrieve executable path: " << GetLastError() << "\n";
+        return "";
+      }
+    }
+    return realpath.get();
+  }();
+#else
+  constexpr char kPathSep = '/';
+#if defined(__APPLE__)
+  std::unique_ptr<char[]> buf(nullptr);
+  {
+    std::uint32_t buf_size = 0;
+    _NSGetExecutablePath(nullptr, &buf_size);
+    buf.reset(new char[buf_size]);
+    if (!buf) {
+      std::cerr << "cannot allocate memory to store executable path\n";
+      return "";
+    }
+    if (_NSGetExecutablePath(buf.get(), &buf_size)) {
+      std::cerr << "unexpected error from _NSGetExecutablePath\n";
+    }
+  }
+  const char* path = buf.get();
+#else
+  const char* path = "/proc/self/exe";
+#endif
+  std::string realpath = [&]() -> std::string {
+    std::unique_ptr<char[]> realpath(nullptr);
+    std::uint32_t buf_size = 128;
+    bool success = false;
+    while (!success) {
+      realpath.reset(new(std::nothrow) char[buf_size]);
+      if (!realpath) {
+        std::cerr << "cannot allocate memory to store executable path\n";
+        return "";
+      }
+
+      std::size_t written = readlink(path, realpath.get(), buf_size);
+      if (written < buf_size) {
+        realpath.get()[written] = '\0';
+        success = true;
+      } else if (written == -1) {
+        if (errno == EINVAL) {
+          // path is already not a symlink, just use it
+          return path;
+        }
+
+        std::cerr << "error while resolving executable path: " << strerror(errno) << '\n';
+        return "";
+      } else {
+        // realpath is too small, grow and retry
+        buf_size *= 2;
+      }
+    }
+    return realpath.get();
+  }();
+#endif
+
+  if (realpath.empty()) {
+    return "";
+  }
+
+  for (std::size_t i = realpath.size() - 1; i > 0; --i) {
+    if (realpath.c_str()[i] == kPathSep) {
+      return realpath.substr(0, i);
+    }
+  }
+
+  // don't scan through the entire file system's root
+  return "";
+}
+
+void scanPluginLibraries(std::shared_ptr<rclcpp::Node> node) {
+  // check and print plugins that are linked directly into the executable
+
+  // define platform-specific strings
+#if defined(_WIN32) || defined(__CYGWIN__)
+  const std::string sep = "\\";
+#else
+  const std::string sep = "/";
+#endif
+
+
+  // try to open the ${EXECDIR}/MUJOCO_PLUGIN_DIR directory
+  // ${EXECDIR} is the directory containing the simulate binary itself
+  // MUJOCO_PLUGIN_DIR is the MUJOCO_PLUGIN_DIR preprocessor macro
+  const std::string executable_dir = getExecutableDir();
+  if (executable_dir.empty()) {
+    return ;
+  }
+  const std::string plugin_dir = getExecutableDir() + sep + MUJOCO_PLUGIN_DIR;
+
+  RCLCPP_INFO_STREAM(node->get_logger(), "Plugin dir: " + plugin_dir);
+
+  mj_loadAllPluginLibraries(plugin_dir.c_str(),
+      +[](const char* filename, int first, int count) {
+        // log += "Plugins registered by library '" + (std::string)filename + "':\n";
+        std::printf("Plugins registered by library '%s':\n", filename);
+        for (int i = first; i < first + count; ++i) {
+          // log += "    " + (std::string)mjp_getPluginAtSlot(i)->name + "\n";
+          std::printf("    %s\n", mjp_getPluginAtSlot(i)->name);
+        }
+      }
+  );
+
+  std::string log = "";
+  int nplugin = mjp_pluginCount();
+  if (nplugin) {
+    log += "Registered plugins:\n";
+    for (int i = 0; i < nplugin; ++i) {
+      log +=  "    " + (std::string)mjp_getPluginAtSlot(i)->name + "\n";
+    }
+  }
+  RCLCPP_INFO_STREAM(node->get_logger(), log);
+  //   return log;
+}
+
+// main function
+int main(int argc, const char **argv)
+{
+  rclcpp::init(argc, argv);
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared(
+    "mujoco_ros2_control_node",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true));
+
+  RCLCPP_INFO_STREAM(node->get_logger(), "Initializing mujoco_ros2_control node...");
+  auto model_path = node->get_parameter("mujoco_model_path").as_string();
+
+  // RCLCPP_INFO_STREAM(node->get_logger(),());
+  std::printf("Scanning for Libraries");
+  RCLCPP_INFO_STREAM(node->get_logger(), "Scanning for libraries");
+  scanPluginLibraries(node);
+
+  // load and compile model
+  char error[1000] = "Could not load binary model";
+  if (
+    std::strlen(model_path.c_str()) > 4 &&
+    !std::strcmp(model_path.c_str() + std::strlen(model_path.c_str()) - 4, ".mjb"))
+  {
+    mujoco_model = mj_loadModel(model_path.c_str(), 0);
+  }
+  else
+  {
+    mujoco_model = mj_loadXML(model_path.c_str(), 0, error, 1000);
+  }
+  if (!mujoco_model)
+  {
+    mju_error("Load model error: %s", error);
+  }
+
+  RCLCPP_INFO_STREAM(node->get_logger(), "Mujoco model has been successfully loaded !");
+  // make data
+  mujoco_data = mj_makeData(mujoco_model);
+
+  // initialize mujoco control
+  auto mujoco_control = mujoco_ros2_control::MujocoRos2Control(node, mujoco_model, mujoco_data);
+
+  mujoco_control.init();
+  RCLCPP_INFO_STREAM(
+    node->get_logger(), "Mujoco ros2 controller has been successfully initialized !");
+
+  // initialize mujoco visualization environment for rendering and cameras
+  if (!glfwInit())
+  {
+    mju_error("Could not initialize GLFW");
+  }
+  auto rendering = mujoco_ros2_control::MujocoRendering::get_instance();
+  rendering->init(mujoco_model, mujoco_data);
+  RCLCPP_INFO_STREAM(node->get_logger(), "Mujoco rendering has been successfully initialized !");
+
+  auto cameras = std::make_unique<mujoco_ros2_control::MujocoCameras>(node);
+  cameras->init(mujoco_model);
+
+  // run main loop, target real-time simulation and 60 fps rendering with cameras around 6 hz
+  mjtNum last_cam_update = mujoco_data->time;
+  while (rclcpp::ok() && !rendering->is_close_flag_raised())
+  {
+    // advance interactive simulation for 1/60 sec
+    //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
+    //  this loop will finish on time for the next frame to be rendered at 60 fps.
+    //  Otherwise add a cpu timer and exit this loop when it is time to render.
+    mjtNum simstart = mujoco_data->time;
+    while (mujoco_data->time - simstart < 1.0 / 60.0)
+    {
+      mujoco_control.update();
+    }
+    rendering->update();
+
+    // Updating cameras at ~6 Hz
+    // TODO(eholum): Break control and rendering into separate processes
+    if (simstart - last_cam_update > 1.0 / 6.0)
+    {
+      cameras->update(mujoco_model, mujoco_data);
+      last_cam_update = simstart;
+    }
+  }
+
+  rendering->close();
+  cameras->close();
+
+  // free MuJoCo model and data
+  mj_deleteData(mujoco_data);
+  mj_deleteModel(mujoco_model);
+
+  return 1;
+}
